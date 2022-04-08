4a111efb0166a6334fa2fa36f6f479c0c9c44fca
Amil Ramesh Rubasinghe <amilrubasinghe@gmail.com>
2022-04-08 09:49:47 +0530

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      color="indigo lighten-4"
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <nuxt-link to="/">
+        <v-btn icon>
+          <v-icon>mdi-logout</v-icon>
+        </v-btn></nuxt-link
+      >
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }} AIMind team</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      items: [
+        {
+          icon: "mdi-apps",
+          title: "MOH Details",
+          to: "/moh",
+        },
+        {
+          icon: "mdi-chart-bubble",
+          title: "PHI Details",
+          to: "/phi",
+        },
+      ],
+      miniVariant: false,
+      title: "AImind chatbot Admin pannel",
+    };
+  },
+};
+</script>
